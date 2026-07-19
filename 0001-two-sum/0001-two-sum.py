e1e7267c9143971ac64fa46eb32409f75f55d3c7
Kunal_Mahajan <90593110+StealBird@users.noreply.github.com>
2026-07-19 11:51:55 +0530

    Time: 0 ms (100%), Space: 13.1 MB (66.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,27 +2,8 @@ class Solution(object):
     def twoSum(self, nums, target):
         hash = {}
         for i,num in enumerate(nums):
-            if num not in hash:
-                hash[num] = [i]
-            else:
-                hash[num].append(i)
-        
-        nums = sorted(nums)
-        n = len(nums)
-        left = 0
-        right = n-1
-
-        while left < right:
-            sum = nums[left] + nums[right]
-            
-            if sum < target:
-                left += 1
-            elif sum > target:
-                right -= 1
-            else:
-                if nums[left] == nums[right]:
-                    return [hash[nums[left]][0], hash[nums[right]][1]]
-                else:
-                    return [hash[nums[left]][0], hash[nums[right]][0]]
-        return [-1,-1]
-          
+            complement = target - num
+            if complement in hash:
+                return [hash[complement], i]
+            hash[num] = i
+        return [-1, -1]
