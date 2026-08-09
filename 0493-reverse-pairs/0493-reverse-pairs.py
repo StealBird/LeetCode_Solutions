870319ae149607d739438ecfb35b29f18d24029b
Kunal_Mahajan <90593110+StealBird@users.noreply.github.com>
2026-08-09 23:23:53 +0530

    Time: 1007 ms (23.42%), Space: 26.3 MB (97.68%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -0,0 +1,52 @@
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        n = len(nums)
+        return self.mergeSort(nums,0,n-1)
+        
+
+    def mergeSort(self, nums, low, high) -> int:
+        cnt = 0
+        if low >= high:
+            return cnt 
+        mid = (low + high) // 2
+        cnt += self.mergeSort(nums,low,mid)
+        cnt += self.mergeSort(nums,mid+1,high)
+        cnt += self.countPairs(nums,low,mid,high)
+        self.merge(nums, low, mid, high)
+        return cnt
+
+
+    def countPairs(self, nums, low, mid, high) -> int:
+        right = mid + 1
+        cnt = 0
+        for i in range(low, mid+1):
+            while (right <= high and nums[i] > 2* nums[right]):
+                right += 1
+            cnt += (right -(mid + 1))
+        return cnt
+          
+
+    def merge(self, nums, low, mid, high):
+        temp =[]
+        left = low
+        right = mid+1
+
+        while left <= mid and right <= high:
+            if nums[left] <= nums[right]:
+                temp.append(nums[left])
+                left += 1
+
+            else:
+                temp.append(nums[right])
+                right += 1
+        while left <= mid:
+            temp.append(nums[left])
+            left += 1
+
+        while right <= high:
+            temp.append(nums[right])
+            right += 1
+        
+        for i in range(low, high+1):
+            nums[i] = temp[i-low]
+        
