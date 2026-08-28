49b5bbc66a09c82e7cdf626ec12ff49e99cae6e9
StealBird_Phoenix <90593110+StealBird@users.noreply.github.com>
2026-08-28 22:57:10 +0530

    Time: 7 ms (31.4%), Space: 19.2 MB (87.74%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -6,17 +6,20 @@ class Solution:
         if x==1:
             return 1
 
-        start = 1
-        end = x
-
-        while(start<=end):
-            mid = start + (end-start) // 2
-
-            if ((mid*mid)==x):
-                return mid
-            elif ((mid*mid) < x):
-                start = mid+1
-            else:
-                end = mid-1
-        return round(end)
+        low = 1
+        high = x
         
+
+        while(low <= high):
+            
+            mid = low +(high - low)//2
+            
+
+            if mid*mid == x:
+                return mid
+            elif mid*mid < x:
+                ans = mid
+                low = mid+1
+            else:
+                high = mid - 1
+        return high
